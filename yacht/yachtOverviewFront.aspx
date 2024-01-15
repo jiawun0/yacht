3ca235dd9471e8029b9fba6c827dc3210dbcffa8
jiawun <ss10232301995@gmail.com>
2024-01-15 20:45:17 +0800

    feature-Dimensions-imgnotok

--- yacht/yachtOverviewFront.aspx
+++ yacht/yachtOverviewFront.aspx
@@ -1,5 +1,39 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Newoverview.Master" AutoEventWireup="true" CodeBehind="yachtOverviewFront.aspx.cs" Inherits="yacht.yachtOverviewFront" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="box1">
+        <asp:Literal ID="Literal_overviewContentHtml" runat="server"></asp:Literal>
+    </div>
+    <div class="box3" id="dimensionTable" runat="server">
+        <asp:Literal ID="Literal_dimensionTitle" runat="server"></asp:Literal>
+        <asp:Literal ID="Literal_Dimension" runat="server"></asp:Literal>
+        <asp:Literal ID="Literal_overviewDimensionsImgPath" runat="server"></asp:Literal>
+        
+        <%--<img src='<%# GetRelativeImagePath(Eval("overviewDimensionsImgPath").ToString()) %>' class="image0" alt="" style="height: 59px;" />--%>
+        <%--<table class="table02">
+            <tbody>
+                <tr>
+                    <td class="table02td01">
+                        <table>
+                            <tbody>
+                                <asp:Literal ID="Literal_Dimension" runat="server"></asp:Literal>
+                            </tbody>
+                        </table>
+                    </td>
+                    <asp:Literal ID="Literal_overviewDimensionsImgPath" runat="server"></asp:Literal>
+                </tr>
+            </tbody>
+        </table>--%>
+    </div>
+    <%--<p class="topbuttom"><img src="image/top.gif" /></p>--%>
+    <div id="divDownload" class="downloads" runat="server">
+        <%--<p><img src="image/downloads.gif" alt="&quot;&quot;" /></p>--%>
+        <ul>
+            <li>
+                <asp:Literal ID="Literal_overviewDownloadsFilePath" runat="server"></asp:Literal>
+            </li>
+        </ul>
+    </div>
 </asp:Content>
--- yacht/yachtOverviewFront.aspx.cs
+++ yacht/yachtOverviewFront.aspx.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Text;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -11,7 +14,81 @@ namespace yacht
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //會先跑 Content 頁的 Page_Load 才跑 Master 頁的 Page_Load
+            if (!IsPostBack)
+            {
+                loadOverviewContent();
+            }
+        }
 
+        private void loadOverviewContent()
+        {
+            //取得網址傳值的 id 內容
+            string guidStr = Session["guid"].ToString();
+
+            //從資料庫取資料
+            SqlConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["ConnectYachtall"].ConnectionString);
+            //string sql = "select overviewContentHtml from Yachts where guid = @guid ";
+            string sql = "select Y.Id, Y.yachtModel, Y.guid, Y.overviewContentHtml, Y.overviewDimensionsImgPath, Y.overviewDownloadsFilePath, YD.Specification, YD.size, YD.YachtsId from Yachts Y" +
+               " left join YachtsDimension YD on Y.Id = YD.YachtsId " +
+               " where guid = @guid ";
+            SqlCommand command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@guid", guidStr);
+            connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+            StringBuilder dimensions = new StringBuilder();
+            StringBuilder dimensionsImgPathHTML = new StringBuilder();
+
+            while (reader.Read())
+            {
+                string yachtModelStr = reader["yachtModel"].ToString();
+                string dimensionsImgPathStr = reader["overviewDimensionsImgPath"].ToString();
+                // Assuming your web application is in the root directory
+                string relativePath = GetRelativeImagePath(dimensionsImgPathStr);
+
+                string downloadsFilePathStr = reader["overviewDownloadsFilePath"].ToString();
+
+                string Specification = reader["Specification"].ToString();
+                string size = reader["size"].ToString();
+
+                dimensions.Append("<table class='table02'>");
+                dimensions.Append("<tbody>");
+
+                // Assuming Specification and Size are variables with content
+                dimensions.Append("<tr><td style='width:300px;'>" + Specification + "</td>");
+                dimensions.Append("<td style='width:300px;'>" + size + "</td></tr>");
+
+                dimensions.Append("</tbody>");
+                dimensions.Append("</table>");
+
+                //渲染CKeditor
+                Literal_overviewContentHtml.Text = HttpUtility.HtmlDecode(reader["overviewContentHtml"].ToString());
+                //遊艇名稱
+                Literal_dimensionTitle.Text = yachtModelStr + " DIMENSIONS";
+                //尺寸表格圖片
+                dimensionsImgPathHTML.Append("<img src = '" + relativePath + "' alt = '' />");
+                //Literal_overviewDimensionsImgPath.Text = dimensionsImgPathHTML.ToString();
+                //下載檔案
+                Literal_overviewDownloadsFilePath.Text = downloadsFilePathStr.ToString();
+
+                //渲染尺寸表格文字內容
+                Literal_Dimension.Text = dimensions.ToString();
+                
+            }
+            
+            connection.Close();
+        }
+
+        //相對路徑
+        protected string GetRelativeImagePath(string Path) //相對路徑
+        {
+            if (!string.IsNullOrEmpty(Path))
+            {
+                string relativePath = Path.Replace(Server.MapPath("~"), "").Replace(Server.MapPath("\\"), "/");
+                return relativePath;
+            }
+            return string.Empty;
         }
     }
 }
