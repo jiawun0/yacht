cb01740e7a0c26999912b4c99c4ba62608f46e43
jiawun <ss10232301995@gmail.com>
2024-01-16 10:58:43 +0800

    update-Specification-ok

--- yacht/yachtSpecificationFront.aspx
+++ yacht/yachtSpecificationFront.aspx
@@ -1,5 +1,13 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Newoverview.Master" AutoEventWireup="true" CodeBehind="yachtSpecificationFront.aspx.cs" Inherits="yacht.yachtSpecificationFront" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="box5">
+        <h4>DETAIL SPECIFICATION</h4>
+
+        <p>HULL STRUCTURE & DECKS</p>
+        <ul>
+            <asp:Literal ID="Literal_specificationContentHtml" runat="server"></asp:Literal>
+        </ul>
 </asp:Content>
--- yacht/yachtSpecificationFront.aspx.cs
+++ yacht/yachtSpecificationFront.aspx.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -11,7 +13,37 @@ namespace yacht
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //會先跑 Content 頁的 Page_Load 才跑 Master 頁的 Page_Load
+            if (!IsPostBack)
+            {
+                loadSpecification();
+            }
+        }
 
+        private void loadSpecification()
+        {
+            //取得網址傳值的 id 內容
+            string guidStr = Session["guid"].ToString();
+
+            //從資料庫取資料
+            SqlConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["ConnectYachtall"].ConnectionString);
+            //string sql = "select overviewContentHtml from Yachts where guid = @guid ";
+            string sql = "select Y.Id, Y.yachtModel, Y.guid, Y.overviewContentHtml, Y.overviewDimensionsImgPath, Y.overviewDownloadsFilePath, Y.specificationContentHtml, YD.Specification, YD.size, YD.YachtsId from Yachts Y" +
+               " left join YachtsDimension YD on Y.Id = YD.YachtsId " +
+               " where guid = @guid ";
+            SqlCommand command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@guid", guidStr);
+            connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            if (reader.Read())
+            {
+                //渲染CKeditor
+                Literal_specificationContentHtml.Text = HttpUtility.HtmlDecode(reader["specificationContentHtml"].ToString());
+            }
+
+            connection.Close();
         }
     }
 }
